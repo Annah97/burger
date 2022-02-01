47deebe2d860a2a1d790e56bf16f560d60913f17
Annah97 <rapelangsekilo@gmail.com>
2022-02-01 09:42:08 +0200

    add burger app files

---
+++ src/App.js
@@ -0,0 +1,52 @@
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+function TopBread() {
+  return <div className="Burger-top"></div>
+}
+function Tomato() {
+  return <div className="Burger-tomato">Tomato</div>
+}
+function Meat() {
+  return <div className="Burger-meat">Meat</div>
+}
+function Lettuce() {
+  return <div className="Burger-lettuce">Lettuce</div>
+}
+function BaseBread() {
+  return <div className="Burger-base"></div>
+}
+export default function Burger() {
+  return (
+    <section>
+      <TopBread />
+      <Tomato />
+      <Meat />
+      <Lettuce />
+      <BaseBread />
+    </section>
+  )
+}
